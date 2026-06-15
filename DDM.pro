7ea68b4502e4e7c9aba07c9cfb73f2637de304ea
Lucio <rodrilu1620@gmail.com>
2026-06-15 15:20:28 -0300

    Agrego haSessionState.h y lo incluyo en commandContext

--- DDM.pro
+++ DDM.pro
@@ -76,6 +76,7 @@ HEADERS += \
     src/model/2w/twoWStationTable.h \
     src/model/commandContext.h \
     src/model/cpa.h \
+    src/model/ha/haSessionState.h \
     src/model/pppcalculator.h \
     src/model/estacionamientocalculator.h \
     src/model/decoders/concDecoder.h \
--- src/model/commandContext.h
+++ src/model/commandContext.h
@@ -19,6 +19,8 @@
 #include "entities/circleEntity.h"
 #include "entities/polygonoentity.h"
 #include "model/2w/twowsessionstate.h"
+#include "ha/haSessionState.h"
+
 
 struct CommandContext {
     enum MotionMode {
@@ -104,6 +106,7 @@ struct CommandContext {
     std::map<int, StationingSession> stationingSessions;
 
     TwoWSessionState twoWSession;
+    HaSessionState   haSession;
 
     double centerX = 0.0;
     double centerY = 0.0;
