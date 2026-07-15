ac1b3eb5c44b8dee2c1713060ede30a51caa4de8
Facundo Diz <71288527+FacundoDiz@users.noreply.github.com>
2026-07-15 15:13:11 -0300

    fix info command

--- src/controller/commands/fondeoCommand.cpp
+++ src/controller/commands/fondeoCommand.cpp
@@ -42,8 +42,8 @@ CommandResult FondeoCommand::execute(const CommandInvocation& inv, CommandContex
         response += QStringLiteral("--> AZ. RELATIVO: %1 grados | DIST RELATIVA: %2\n")
                         .arg(s.azimutRelativo, 0, 'f', 1).arg(s.distanciaRelativa, 0, 'f', 1);
         response += QStringLiteral("------------------------------------------------------\n");
-        response += QStringLiteral("MOV. ACTUAL: %1\n").arg(s.movimientoActual);
-        response += QStringLiteral("PROXIMO MOV: %1\n").arg(s.proximoMovimiento);
+        response += QStringLiteral("MOV. ACTUAL: %1 (%2 yds)\n").arg(s.movimientoActual.label).arg(s.movimientoActual.distancia, 0, 'f', 1);
+        response += QStringLiteral("PROXIMO MOV: %1 (%2 yds)\n").arg(s.proximoMovimiento.label).arg(s.proximoMovimiento.distancia, 0, 'f', 1);
         response += QStringLiteral("======================================================\n\n");
 
         return { true, response };
